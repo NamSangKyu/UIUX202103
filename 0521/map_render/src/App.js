c421684807b918cdde3947c2d974494e57b39410
NamSangKyu <nam2626@gmail.com>
2021-05-21 11:33:03 +0900

    생성될 컴포넌트 CSS 변경하기

---
+++ 0521/map_render/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+class Circle extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    var line = ['1호선', '2호선', '3호선', '4호선', '5호선'];
+    var color = ['#0B0B61','#0B610B','#FF8000','#2ECCFA','#BF00FF'];
+    var style = {
+      "width" : "100px",
+      "height" : "100px",
+      "box-sizing" : "border-box",
+      "padding": "35px 0px",
+      "text-align" : "center",
+      "line-height" : "20px",
+      "font-size" : "20px",
+      "border-radius" : "50px",
+      "border" : "5px solid red"
+    };
+    const lineItems = line.map((data,index) =>{
+      var border = style.border.split(" ");
+      border[2] = color[index];
+      style.border = border.join(" ");
+      return (<div style={style}>{data}</div>);
+    } );
+    return (<div>{lineItems}</div>);
+  }
+
+
+}
+export default Circle;
