264c243cce5bcfd7f1a24542ed83a47ba219a0b3
NamSangKyu <nam2626@gmail.com>
2021-06-15 11:44:12 +0900

    express 작업

---
+++ express/app.js
@@ -0,0 +1,22 @@
+const { request, response } = require('express');
+const express = require('express');
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+var mysql = require('mysql')
+const app = express()
+const port = 3000
+app.listen(port, () =>{
+    console.log(`서버 실행 완료 포트번호 ${port}`)
+})
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+})
+app.all('/',(request,response)=>{
+        response.send("express 서버 접속 완료")
+})
+app.get('/get.do',(request,response)=>{
+    const data = {key1 : "데이터1", key2 : "데이터2"}
+    response.send(JSON.stringify(data))
+})
